8924685660010ea1f159659d526cdc2c6710f410
Vladyslav Yarema <vlad14yarema@gmail.com>
2022-09-23 14:15:29 +0300

    added my solved tasks from Codewars

---
+++ Tasks/Codewars/Python/move_zeros.py
@@ -0,0 +1,16 @@
+# Write an algorithm that takes an array and moves all of the zeros to the end,
+# preserving the order of the other elements.
+# move_zeros([1, 0, 1, 2, 0, 1, 3]) # returns [1, 1, 2, 1, 3, 0, 0]
+def move_zeros(lst):
+    for i in range(lst.count(0)):
+        lst.remove(0)
+        lst.append(0)
+    return lst
+
+
+# another solution
+def move_zeros_1(array):
+    return [x for x in array if x] + [0]*array.count(0)
+
+
+print(move_zeros([1, 0, 1, 2, 0, 1, 3]))
---
+++ Tasks/Codewars/Python/unique_in_order.py
@@ -0,0 +1,17 @@
+# Implement the function unique_in_order which takes as argument a sequence and returns a list of items without
+# any elements with the same value next to each other and preserving the original order of elements.
+# For example:
+# unique_in_order('AAAABBBCCDAABBB') == ['A', 'B', 'C', 'D', 'A', 'B']
+# unique_in_order('ABBCcAD')         == ['A', 'B', 'C', 'c', 'A', 'D']
+# unique_in_order([1,2,2,3,3])       == [1,2,3]
+def unique_in_order(iterable):
+    item = ''
+    item_list = []
+    for i in list(iterable):
+        if i != item:
+            item = i
+            item_list.append(i)
+    return item_list
+
+
+print(unique_in_order(['A', 'B', 'C', 'C', 'D', 'A', 'A', 'B', 'B', 'B']))
