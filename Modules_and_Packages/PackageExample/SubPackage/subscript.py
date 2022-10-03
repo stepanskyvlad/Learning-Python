298e87ec653513a8c7f1ae6f70e68ab1a204aac5
Vladyslav Yarema <vlad14yarema@gmail.com>
2022-10-03 16:59:16 +0300

    updated Modiles and Packages folder

---
+++ Modules_and_Packages/PackageExample/SubPackage/subscript.py
@@ -0,0 +1,6 @@
+def hello_subscript():
+    print('Hello subscript')
+
+
+def hello_indeed():
+    print("hello indeed")
---
+++ Modules_and_Packages/using_package.py
@@ -0,0 +1,8 @@
+from PackageExample import main_script as main
+from PackageExample.SubPackage.subscript import hello_subscript, hello_indeed
+
+
+main.hello_main()
+
+hello_subscript()
+hello_indeed()
