e0a54e0f22678f990c77ba6e78d5ed6eadf11e93
Vladyslav Yarema <vlad14yarema@gmail.com>
2022-10-14 08:06:59 +0300

    added new solved tasks

---
+++ Tasks/Codewars/building_blocks.py
@@ -0,0 +1,8 @@
+class Block:
+    def __init__(self, width, length, height):
+        self.width = width
+        self.length = length
+        self.height = height
+
+    def get_width(self):
+        pass
