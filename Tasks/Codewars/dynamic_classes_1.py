cf22900e9d139165d9dc02971fa623fe30c44541
Vladyslav Yarema <vlad14yarema@gmail.com>
2022-10-20 16:34:54 +0300

    added solved tasks

---
+++ Tasks/Codewars/dynamic_classes_1.py
@@ -0,0 +1,15 @@
+class ClassNameError(Exception):
+    pass
+
+
+def class_name_changer(cls, new_name):
+    if new_name.isalnum() and new_name[0].isupper():
+        cls.__name__ = new_name
+    else:
+        raise ClassNameError("Invalid class name")
+
+
+# another solution
+def class_name_changer_1(cls, new_name):
+    assert new_name[0].isupper() and new_name.isalnum()
+    cls.__name__ = new_name
---
+++ Tasks/Codewars/quarks.py
@@ -0,0 +1,28 @@
+class Quark(object):
+    def __init__(self, color, flavor):
+        self.color = color
+        self.flavor = flavor
+        self.baryon_number = 1/3
+
+    def interact(self, other):
+        self.color, other.color = other.color, self.color
+
+
+# another solution
+class QuarkOne(object):
+    _COLOR = frozenset(["red", "blue", "green"])
+    _FLAVOR = frozenset(['up', 'down', 'strange', 'charm', 'top', 'bottom'])
+    baryon_number = 1 / 3
+
+    def __init__(self, color, flavor):
+        if color in self._COLOR:
+            self.color = color
+        else:
+            raise ValueError(f"{color} is not allowed color for quarks!")
+        if flavor in self._FLAVOR:
+            self.flavor = flavor
+        else:
+            raise ValueError(f"{flavor} is not allowed flavor for quarks!")
+
+    def interact(self, quark):
+        self.color, quark.color = quark.color, self.color
