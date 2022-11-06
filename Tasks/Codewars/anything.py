5bab4e4f2c92cceb52a9265a5590c4cd97cd0399
Vladyslav Yarema <vlad14yarema@gmail.com>
2022-11-06 13:44:03 +0200

    added three OOP solved tasks from Codewars

---
+++ Tasks/Codewars/anything.py
@@ -0,0 +1,30 @@
+class Thing:
+    def __init__(self, something):
+        self.something = something
+
+    def __eq__(self, other):
+        return True
+
+    def __ne__(self, other):
+        return True
+
+    def __le__(self, other):
+        return True
+
+    def __ge__(self, other):
+        return True
+
+    def __lt__(self, other):
+        return True
+
+    def __gt__(self, other):
+        return True
+
+
+def anything(thing):
+    something = Thing(thing)
+    return something
+
+
+bullshit = anything("hello")
+print(bullshit < "hello")
