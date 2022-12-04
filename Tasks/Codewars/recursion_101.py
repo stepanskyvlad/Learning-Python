5afb9ee08e21c043e6ecd196c2911484496923b8
Vladyslav Yarema <vlad14yarema@gmail.com>
2022-12-04 20:37:37 +0200

    added solved tasks from Codewars

---
+++ Tasks/Codewars/recursion_101.py
@@ -0,0 +1,27 @@
+# Recursion 101
+def solve(a, b):
+    if a == 0 or b == 0:
+        return [a, b]
+    elif a >= 2 * b:
+        a = a - 2 * b
+        return solve(a, b)
+    elif b >= 2 * a:
+        b = b - 2 * a
+        return solve(a, b)
+    else:
+        return [a, b]
+
+
+# not recursion
+def solve_1(a, b):
+    while a and b:
+        if a >= b * 2:
+            a %= 2 * b
+        elif b >= a * 2:
+            b %= 2 * a
+        else:
+            break
+    return [a, b]
+
+
+print(solve(2, 10))
---
+++ Tasks/Codewars/replace_a_few_chars.py
@@ -0,0 +1,25 @@
+# Correct the mistakes of the character recognition software
+def correct(s):
+    s = s.replace('5', 'S')
+    s = s.replace('0', 'O')
+    s = s.replace('1', 'I')
+    return s
+
+
+# other solutions
+def correct_1(string):
+    return string.translate(str.maketrans("501", "SOI"))
+
+
+def correct_2(string):
+    return string.replace('1', 'I').replace('0', 'O').replace('5', 'S')
+
+
+def correct_3(s):
+    mapping = str.maketrans({'0': 'O', '1': 'I', '5': 'S'})
+    return s.translate(mapping)
+
+
+def correct_4(string):
+    tr = str.maketrans('015', 'OIS')
+    return string.translate(tr)
