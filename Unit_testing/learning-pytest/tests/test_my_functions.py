23f5924d930cb2e332ca0714275e14ed7959e3ff
Vladyslav Yarema <vlad14yarema@gmail.com>
2024-01-27 18:29:56 +0200

    added examples how to test using pytest

---
+++ Unit_testing/learning-pytest/tests/test_my_functions.py
@@ -0,0 +1,41 @@
+import pytest
+import time
+import source.my_functions as my_functions
+
+
+def test_add():
+    result = my_functions.add(1, 4)
+    assert result == 5
+
+
+def test_add_string():
+    result = my_functions.add("i like ", "burgers")
+    assert result == "i like burgers"
+
+
+def test_divide():
+    result = my_functions.divide(10, 5)
+    assert result == 2
+
+
+def test_divide_by_zero():
+    with pytest.raises(ValueError):
+        my_functions.divide(10, 0)
+
+
+@pytest.mark.slow
+def test_very_slow():
+    time.sleep(5)
+    result = my_functions.divide(10, 5)
+    assert result == 2
+
+
+@pytest.mark.skip(reason="This feature is currently broken")
+def test_add():
+    assert my_functions.add(1, 2) == 3
+
+
+@pytest.mark.xfail(reason="Cannot divide by zero")
+def test_divide_zero_broken():
+    my_functions.divide(4, 0)
+ 
